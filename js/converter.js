3129b0e1515f43b4752fbae477ac568777f49c82
Ewa Marciniak <emarciniak78@gmail.com>
2014-02-26 20:08:50 +0000

    Initial commit

---
+++ js/converter.js
@@ -0,0 +1,8 @@
+var Converter = (function () {
+    // private stuff goes here
+    return {
+        convertFromImperialToMetric: function (weight) {
+            return weight/2.2;
+        }
+    }
+})();
